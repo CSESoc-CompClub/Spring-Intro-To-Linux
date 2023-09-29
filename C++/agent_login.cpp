d472924d39138dcfc9091a9e96cd54dae03d3b1f
Calley Chai <annyi.chai@gmail.com>
2023-09-29 19:57:52 +1000

    feat: starter code for cpp exercises

---
+++ C++/agent_login.cpp
@@ -0,0 +1,20 @@
+// This program takes in a password and checks it to ensure only authorized personnel can log into the system
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Welcome, Agent. Please enter the secret code to proceed: ";
+
+    // TODO[1]: take in input to scan in secret code
+
+
+    // TODO[2]: verify that input given is correct
+    // If it is correct, print a message to grant access
+    // Else we deny access, print an error message
+    // You have the decision to decide what counts as a secret code
+    
+
+    return 0;
+}
---
+++ C++/hello_agent.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    // TODO: type the message to be printed out in the line below
+    // It can be any message you want! One suggestion is printing "hello world"
+    
+    // Do not modify lines below
+    return 0;
+}
